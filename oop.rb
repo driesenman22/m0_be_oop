5aa6d8da6d66fb0c52603f66e2dde49f392694ff
Dylan Riesenman <djriesenman@gmail.com>
2023-03-31 20:37:27 -0600

    Add semi complete oop assignment

--- oop.rb
+++ oop.rb
@@ -5,22 +5,22 @@
 
 
 
-# class Unicorn
+class Unicorn
     
-#     attr_reader :color, :name
-# def initialize(name)
-#     @name = name
-#     @color = "Silver"
-# end
+    attr_reader :color, :name
+def initialize(name)
+    @name = name
+    @color = "Silver"
+end
 
-# def say(statement)
-#     "*~* #{statement} *~*"
-# end
-# end
+def say(statement)
+    "*~* #{statement} *~*"
+end
+end
 
-# unicorn = Unicorn.new ("Greg")
+unicorn = Unicorn.new ("Greg")
 
-# puts u.say("Where can a unicorn get some fairy dust around here??")
+puts u.say("Where can a unicorn get some fairy dust around here??")
 
 
 
@@ -31,26 +31,26 @@
 #  it should have a drink method. When called, the thirsty attribute changes to false
 
 
-# class Vampire
-#     attr_reader :name, :pet, :thirsty
-#     def initialize(name, pet = "bat")
-#         @name = name
-#         @pet =  pet
-#         @thirsty = true
-#     end
+class Vampire
+    attr_reader :name, :pet, :thirsty
+    def initialize(name, pet = "bat")
+        @name = name
+        @pet =  pet
+        @thirsty = true
+    end
 
-#     def drinks_blood
-#         @thirsty = false
-#     end
-# end
+    def drinks_blood
+        @thirsty = false
+    end
+end
 
-# vamp = Vampire.new("Nandor the Relentless")
-# puts vamp.name
-# puts vamp.pet
-# puts vamp.thirsty
+vamp = Vampire.new("Nandor the Relentless")
+puts vamp.name
+puts vamp.pet
+puts vamp.thirsty
 
-# vamp.drinks_blood
-# puts vamp.thirsty
+vamp.drinks_blood
+puts vamp.thirsty
 
 
 
@@ -62,27 +62,27 @@
 #  it should have a eat method. If the dragon eats 4 times, it is no longer hungry
 
 
-# class Dragon
-#     attr_reader :name, :rider, :color :is_hungry
-#     def initialize(name, rider, color)
-#         @name = name
-#         @rider = rider
-#         @color = color
-#         @is_hungry = true
-#     end
+class Dragon
+    attr_reader :name, :rider, :color :is_hungry
+    def initialize(name, rider, color)
+        @name = name
+        @rider = rider
+        @color = color
+        @is_hungry = true
+    end
 
-#     def eat
-#         @is_hungry = false 
-#         if eat = 4
-#         end
-#     end
+    def eat
+        @is_hungry = false 
+        if eat = 4
+        end
+    end
 
-#     dragon1 = Dragon.new("Jeff")
-#     puts dragon1.name
-#    dragon1 = Dragon.new("fast")            # Here I am unsure how to get the console to read different dynamic attributes
-#    puts dragon1.rider                      # I am also unsure how to incorporate the dragon eating 4 meals
-#    dragon1 = Dragon.new("green")
-#    puts dragon1.color
+    dragon1 = Dragon.new("Jeff")
+    puts dragon1.name
+   dragon1 = Dragon.new("fast")            # Here I am unsure how to get the console to read different dynamic attributes
+   puts dragon1.rider                      # I am also unsure how to incorporate the dragon eating 4 meals
+   dragon1 = Dragon.new("green")
+   puts dragon1.color
 
 #  Write a Hobbit class
 #  it should have a dynamic name attribute (string)
@@ -93,7 +93,9 @@
 #  it should have an is_old attribute that defaults to false. once a Hobbit is 101, it is old.
 #  it should have a has_ring attribute. If the Hobbit's name is "Frodo", true, if not, false.
 
-class Hobbit
+
+
+  class Hobbit
     attr_reader :name, :disposition, :age, :is_adult, :is_old, :has_ring
     def initialize(name, disposition)
         @name = name
@@ -115,5 +117,28 @@ class Hobbit
         end
     end
 end
-puts Hobbit.new("Samwise, kind").has_ring
 
+hobbit1 = Hobbit.new("Frodo", "fearless")
+puts hobbit1.name
+puts hobbit1.disposition
+puts hobbit1.age
+puts hobbit1.is_adult
+puts hobbit1.is_old
+puts hobbit1.has_ring
+
+hobbit1.celebrate_birthday
+puts hobbit1.age
+puts hobbit1.is_adult
+puts hobbit1.is_old
+
+31.times { hobbit1.celebrate_birthday }
+puts hobbit1.age
+puts hobbit1.is_adult
+puts hobbit1.is_old
+
+111.times { hobbit1.celebrate_birthday }
+puts hobbit1.age
+puts hobbit1.is_adult
+puts hobbit1.is_old
+
+puts Hobbit.new("Samwise", "Brave").has_ring
